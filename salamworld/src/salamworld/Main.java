6257efe549b3ea15eee523bdcefb04b1580d5ce8
alpha <alpha@kda.com>
2017-09-24 13:11:14 +0100

    quwatro c

--- salamworld/src/salamworld/Main.java
+++ salamworld/src/salamworld/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("salam Rabat");
+		System.out.println("salam Tanger");
 		System.out.println("salam Fès");
+		System.out.println("salam swira");
 	}
 
 }
