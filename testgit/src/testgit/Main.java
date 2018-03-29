15e1f33ca2f7dd32166eb7d96ad27304e6e15832
alpha <alpha@kda.com>
2018-03-29 16:36:16 +0100

    sdqscsqc

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 9;
-		String = "jib had fichier l pc";
+		String prenom = "jib had fichier l pc";
 	}
 
 }
