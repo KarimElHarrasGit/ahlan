24e6a2287a8684fe527172469b7d60b41e5dac63
alpha <alpha@kda.com>
2018-04-09 15:47:08 +0100

    cc

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -4,14 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String alpha="code karim";
-		int a = 9;
-		String nom = "master";
-		int b = 5;
-		int c;
-		int f = 6;
-		int p=36;
-		String prenom = "code anas";
+		String nom="code hadi";
 	}
 
 }
